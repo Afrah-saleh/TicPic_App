f7ac399c0f32b3f8bf19b690e163e5c1a269ac1f
Afrah-saleh <afrahsaleh49@hotmail.com>
2023-11-26 10:08:18 +0300

    Initial Commit

---
+++ TicPic_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicPic_App
+//
+//  Created by Afrah Saleh on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicPic_App/TicPic_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicPic_AppApp.swift
+//  TicPic_App
+//
+//  Created by Afrah Saleh on 12/05/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct TicPic_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
